f03c575fd8ced4d401d9786ed440eac606fc3df8
i5m <i5ivem@gmail.com>
2010-03-16 13:33:55 +0000

    First release candidate of App
    * API Key and Shared secret in external file
    * Separated app into different methods
    * This allows redrawing. If new set is selected
      redraws progress bar and tag cloud
    * Added logout method to clear token
    * Other GUI refinements:
      - Click on username to show/hide token
      - logout button
      - progress bar nudged slightly to right

--- shoeset.rb
+++ shoeset.rb
@@ -7,82 +7,122 @@ require 'yaml'
 require 'cloud'
 
 
-Shoes.app do
+Shoes.app :title => "Shoeset" do
 
 	
-	#Need to improve GUI feedback on opening. Thread??
-	FlickRaw.api_key=""
-	FlickRaw.shared_secret=""
-	
+	KEYFILE = "keys.yml"
+	KEYS = YAML::load(File.read(KEYFILE))
+	FlickRaw.api_key=KEYS["api_key"]
+	FlickRaw.shared_secret=KEYS["shared_secret"]
 	TOKENFILE = ENV['HOME']+"\\.shoeset.yml"
-	#Load token if it exists
-	if File.exists?(TOKENFILE)
-				$SETTINGS = YAML::load(File.read(TOKENFILE))
-				@token = $SETTINGS["Token"]
-				auth = flickr.auth.checkToken :auth_token => @token
-				
-	else
 
-		frob = flickr.auth.getFrob
-		auth_url = FlickRaw.auth_url :frob => frob, :perms => 'read'
-	
-		para "Click to authorise with Flickr : ", link("Authorise", :click =>auth_url)
-		para "Click OK when you are finished."
-		@okbutton = button "OK" do
-			begin
-				auth = flickr.auth.getToken :frob => frob
-				
-				$SETTINGS = { "Token" => auth.token }
-				f = File.open(TOKENFILE, 'w')
-				f.write(YAML.dump $SETTINGS)
-				f.close
-
-			rescue FlickRaw::FailedResponse => e
-				para "Authentication failed : #{e.msg}"
+	#Need to improve GUI feedback/responsive on opening. Thread??
+	def login
+		if File.exists?(TOKENFILE) #Load token if it exists
+					$SETTINGS = YAML::load(File.read(TOKENFILE))
+					@token = $SETTINGS["Token"]
+					@auth = flickr.auth.checkToken :auth_token => @token
+					@login = flickr.test.login
+					@container.clear{ui} #draw ui 		
+		else
+			frob = flickr.auth.getFrob
+			auth_url = FlickRaw.auth_url :frob => frob, :perms => 'read'
+		
+			para "Click to ", link("authorise", :click =>auth_url), " with Flickr."
+			para "Click OK when you are finished."
+			@okbutton = button "OK" do
+				begin
+					@auth = flickr.auth.getToken :frob => frob
+					$SETTINGS = { "Token" => @auth.token }
+					f = File.open(TOKENFILE, 'w')
+					f.write(YAML.dump $SETTINGS)
+					f.close
+					@login = flickr.test.login
+					@container.clear{ui} #draw ui
+				rescue FlickRaw::FailedResponse => e
+					para "Authentication failed : #{e.msg}"
+				end
 			end
 		end
 	end
-	login = flickr.test.login
-	para "You are authenticated as #{login.username} with token #{auth.token}"
-	@setlist = []
-	@photosetlist = flickr.photosets.getList.each do |set|
-		@setlist << set["title"]
+
+
+	def ui
+		flow do
+			para "You are logged in as "
+			para link(@login.username){@token.toggle}
+			button "Logout", :displace_left => 10 do
+				logout
+			end
+		end
+		@token = para "Token: #{@auth.token}"
+		@token.hide
+		@setlist = []
+		@photosetlist = flickr.photosets.getList.each do |set|
+			@setlist << set["title"]
+		end
+		flow do
+			para "Pick a set to generate Tag Cloud: "
+			list_box :items => @setlist, 
+				:chose => @setlist[0] do |set|
+					fluffygen(set)				
+				end
+		end #flow
+		@tagcloud = flow do #if this is a stack then clearing it goes crazy
+			para ""
+		end #set-up placeholder for tag cloud
+	end
+	
+
+	def logout
+		#remove token file
+		File.delete(TOKENFILE)
+		#clear window and restart
+		@container.clear
+		login
 	end
 
-	list_box :items => @setlist, 
-		:chose => @setlist[0] do |set|
-			#Need to implement redraw on next select, etc....
-			para set.text
-			$p = progress :width => 1.0
-			photosetinfo = @photosetlist.select {|s| s["title"] == set.text}
-		
-			debug(photosetinfo)
-			count = photosetinfo[0]["photos"].to_f
-			counter = 0.0
-			debug photosetinfo[0]["id"].to_s #it's an array of a hash. Even though just one
-			photosetphotos = flickr.photosets.getPhotos(:photoset_id => photosetinfo[0]["id"] )
-			debug(photosetphotos["photo"])
-			$array = []
-			Thread.new do
-				photosetphotos["photo"].each do |photo|
-					debug flickr.tags.getListPhoto(:photo_id => photo["id"])
-					temp = flickr.tags.getListPhoto(:photo_id => photo["id"])
-					temp["tags"].each do |tags| #should be an array
-						$array << tags["_content"]
-					end
-					debug $array
-					sleep(1) # Sleep interval between calls.
-					counter += 1.0
-					$p.fraction = counter/count
-					debug (counter/count)
-					# Caching - anyway? Could cache list of photos from set, but then what about if updated?
+	
+	def fluffygen(set)
+		@tagcloud.clear #is this because it's on a global variable that it clears everything???? Nope
+		$p = progress :width => 0.8, :displace_left => 10
+		#debug(set.text)
+		photosetinfo = @photosetlist.select {|s| s["title"] == set.text}
+		#debug(photosetinfo)
+		count = photosetinfo[0]["photos"].to_f
+		counter = 0.0
+		#debug photosetinfo[0]["id"].to_s #it's an array of a hash. Even though just one
+		photosetphotos = flickr.photosets.getPhotos(:photoset_id => photosetinfo[0]["id"] )
+		#debug(photosetphotos["photo"])
+		$array = []
+		Thread.new do
+			photosetphotos["photo"].each do |photo|
+				#debug flickr.tags.getListPhoto(:photo_id => photo["id"])
+				temp = flickr.tags.getListPhoto(:photo_id => photo["id"])
+				temp["tags"].each do |tags| #should be an array
+					$array << tags["_content"]
 				end
+				#debug $array
+				sleep(1) # Sleep interval between calls.
+				counter += 1.0
+				$p.fraction = counter/count
+				#debug (counter/count)
+				# Caching - anyway? Could cache list of photos from set, but then what about if updated?
+			end
+			#debug $array
 			cloud = TagCloud.new($array.join(" "))
 			$p.hide
-			debug cloud.build
-			eval cloud.build
-			end
+			#debug cloud.build
+			@tagcloud.clear{eval cloud.build}
 		end
+	end
+
+
+	@container = slot do
+		para "Never see this"
+	end
+	login
+
 end
 
 
